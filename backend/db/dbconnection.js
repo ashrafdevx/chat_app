8c42a116916a5bd77df1a94bfe072c2e71cc4fc5
ashrafbangash <ashraf.devx@gmail.com>
2025-07-17 00:44:02 +0500

    error resoleed

--- backend/db/dbconnection.js
+++ backend/db/dbconnection.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const dbConnection = async () => {
   try {
+    console.log("process.env.DB_CONNECT", process.env.DB_CONNECT);
     await mongoose.connect(process.env.DB_CONNECT);
     console.log("Connected To DB!!!");
   } catch (error) {
